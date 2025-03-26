7710750f118558cf039dd4092cb47747bbccdd2d
Shubh <shubh2268@gmail.com>
2025-03-26 20:43:00 +0530

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Link } from 'react-router';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+    return (
+        <nav className='bg-white shadow-md'>
+            <div className='container mx-auto px-8 md:px-14 py-3 flex justify-between items-center'>
+                {/* Logo */}
+                <Link to='/' className='text-3xl font-bold text-green-400 flex items-center px-3 py-1'>Pixora</Link>
+
+                {/* Mobile Menu Button */}
+                <button className='md:hidden text-black text-2xl' onClick={() => setMenuOpen(!menuOpen)}>
+                    {menuOpen ? <FiX /> : <FiMenu />}
+                </button>
+
+                {/* Navigation Links */}
+                <div
+                    className={`absolute md:relative top-16 md:top-auto left-0 w-full md:w-auto shadow-md md:shadow-none transition-all duration-300 ease-in-out transform origin-top ${menuOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0'} md:opacity-100 md:scale-y-100 md:flex md:items-center md:gap-6 p-6 md:p-0`}>
+
+                    <Link to='/' className='block text-green-400 font-medium py-2 md:py-0 text-center hover:text-green-500' onClick={() => setMenuOpen(false)}>
+                        Home
+                    </Link>
+
+                    <Link to='/favorites' className='block text-green-400 font-medium py-2 md:py-0 text-center hover:text-green-500' onClick={() => setMenuOpen(false)}>
+                        Favorites
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
