4c95a7df235cb247ed3ab11465d0ee581073286e
Shubh <shubh2268@gmail.com>
2025-03-28 17:18:32 +0530

    --

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,21 +1,40 @@
-const demoImages = [
-    'https://images.unsplash.com/photo-1741851374411-9528e6d2f33f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1741851374411-9528e6d2f33f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1741851374411-9528e6d2f33f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1741851374411-9528e6d2f33f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1741851374411-9528e6d2f33f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-];
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Gallery = () => {
+    const { images, loading } = useContext(AppContext);
+
     return (
         <section className='container mx-auto px-6 py-10'>
-            <h2 className='text-3xl font-bold text-center text-gray-800 mb-6'>Trending Images</h2>
+            <h2 className='text-3xl font-bold text-center text-gray-800 mb-6'>
+                Trending Images
+            </h2>
 
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                {demoImages.map((src, index) => (
-                    <img key={index} src={src} alt='Demo' className='w-full h-auto rounded-md shadow-md' />
-                ))}
-            </div>
+            {loading ? (
+                <p className='text-center text-gray-500'>Loading...</p>
+            ) : (
+                <div className='columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4'>
+                    {images.map((image) => {
+                        const tagsArray = image.tags.split(',').slice(0, 5);
+
+                        return (
+                            <div key={image.id} className='relative overflow-hidden shadow-md group'>
+                                {/* Image */}
+                                <img src={image.webformatURL} alt={image.tags}className='w-full h-auto object-cover transition-transform duration-300 ease-in-out group-hover:scale-105'/>
+
+                                {/* Tags */}
+                                <div className='absolute bottom-3 left-2 flex flex-wrap gap-0.5 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                                    {tagsArray.map((tag, index) => (
+                                        <span key={index} className='text-gray-100 text-sm md:text-base m-1 '>
+                                            {tag.trim()}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
         </section>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router';
 import { FiMenu, FiX } from 'react-icons/fi';
+import { FaCameraRetro } from 'react-icons/fa';
 import { MdOutlineHome, MdFavoriteBorder } from 'react-icons/md';
 import { useState } from 'react';
 
@@ -7,22 +8,25 @@ const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className='w-full z-20 fixed'>
+        <nav className='w-full z-20 sticky bg-green-400/50'>
             <div className='px-5 flex items-center justify-between w-full h-full'>
 
                 {/* Logo */}
                 <div className='flex items-center text-white'>
-                    <Link to='/' className='text-2xl sm:text-3xl font-bold py-1 px-3 md:m-2'>pixora</Link>
+                    <Link to='/' className='flex items-center text-2xl sm:text-3xl font-bold py-1 px-3 md:m-2'>
+                        <span>pixora</span>
+                        <FaCameraRetro size={25} />
+                    </Link>
                 </div>
 
                 <ul className='hidden md:flex'>
                     <Link to='/' className='flex items-center justify-center m-2 font-medium text-white hover:text-green-300'>
                         <MdOutlineHome size={20} />
-                        <span>Home</span>
+                        <span className='ml-1'>Home</span>
                     </Link>
                     <Link to='/favorites' className='flex items-center m-2 font-medium text-white hover:text-green-300'>
-                        <MdFavoriteBorder size={20} />
-                        <span>Favorites</span>
+                        <MdFavoriteBorder size={18} />
+                        <span className='ml-1'>Favorites</span>
                     </Link>
                 </ul>
 
